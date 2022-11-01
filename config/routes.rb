d006a60e29b8bca1962ba3a62cb5b3915fbb8e61
kkosuke <kkosuke007@gmail.com>
2022-11-01 23:20:50 +0900

    お試しページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+  # 適当ページのroutingです。
+  get "/test" => "tests#index"
 end
